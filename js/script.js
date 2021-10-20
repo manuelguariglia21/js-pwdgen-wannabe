835e85d37e76dd086b95a9716a69dab0d75de760
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-20 14:31:11 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 //creo variabili nome, cognome, colore
 
 const firstname = prompt("inserisci il tuo nome: ");
-console.log('Name: ', firstname)
+console.log('Name: ', firstname);
 const lastname = prompt("inserisci il tuo cognome: ");
-console.log('Lastname: ', lastname)
+console.log('Lastname: ', lastname);
 const color = prompt("inserisci il tuo colore preferito: ");
-console.log('Color: ', color)
+console.log('Color: ', color);
+
+//creo variabile password
+
+const yourpass = firstname + lastname + color + 21;
+
+//stampo la password
+
+document.getElementById('your-password').innerHTML = 
+`
+<h2>La tua password è: ${yourpass}</h2>
+
+`;
