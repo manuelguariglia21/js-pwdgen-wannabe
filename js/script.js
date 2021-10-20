335f3e90eb32b2d599226cf7975860eaf8f155b9
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-20 14:23:57 +0200

    fix variable input

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//creo variabili nome, cognome, colore
+
+const firstname = prompt("inserisci il tuo nome: ");
+console.log('Name: ', firstname)
+const lastname = prompt("inserisci il tuo cognome: ");
+console.log('Lastname: ', lastname)
+const color = prompt("inserisci il tuo colore preferito: ");
+console.log('Color: ', color)
